35b01ef8792fddeb0a8a3b6ac4e69a407e4cb87d
Mondotrasho <oscar@peoplepictures.com.au>
2018-06-18 17:14:36 +1000

    Add project files.

---
+++ MathsApp/MathsApp.cpp
@@ -0,0 +1,8 @@
+#include "Utility.h"
+#include <iostream>
+int main()
+{
+	std::cout << add(10, 5) << std::endl;
+	system("pause");
+	return 0;
+}
---
+++ MathsLib/Vector3.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class Vector3 {
+public:
+	union {
+		//struct as xyz accessable with . operator
+		struct {
+			float x, y, z;
+		};
+		//array accessable with [] operator
+		float data[3];
+	};
+
+	float operator[](int index) const; //getter
+	float& operator[](int index);      //setter
+	Vector3 operator+(const Vector3& other) const;
+	Vector3& operator-=(const Vector3& other);
+	Vector3 operator*(float scalar) const;
+	Vector3& operator/=(float scalar);
+	Vector3& operator=(const Vector3& other);
+};
