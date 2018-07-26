3a6685e4ac1d522dcc138291047047db5d9ef644
Mondotrasho <oscar@peoplepictures.com.au>
2018-07-26 20:49:11 +1000

    added ray class

---
+++ MathsLib/Ray.cpp
@@ -0,0 +1,99 @@
+#include "Ray.h"
+#include "Utilities.h"
+#include "Sphere.h"
+#include "Plane.h"
+
+
+Vector2 Ray::closestPoint(const Vector2& point) const {
+	// ray origin to arbitrary point
+	auto p = point - origin;
+	// project the point onto the ray and clamp by length
+	float t = clamp(p.dot(direction), 0, length);
+	// return position in direction of ray
+	return origin + direction * t;
+}
+bool Ray::intersects(const Sphere& sphere,
+	Vector2* I = nullptr) const {
+	// ray origin to sphere center
+	auto L = sphere.center - origin;
+	// project sphere center onto ray
+	float t = L.dot(direction);
+	// get sqr distance from sphere center to ray
+	float dd = L.dot(L) - t * t;
+	// subtract penetration amount from projected distance
+	t -= sqrt(sphere.radius * sphere.radius - dd);
+	// it intersects if within ray length
+	if (t >= 0 && t <= length) {
+		// store intersection point if requested
+		if (I != nullptr)
+			*I = origin + direction * t;
+		return true;
+	}
+	// default no intersection
+	return false;
+}
+bool Ray::intersects(const Plane& plane,Vector2* I = nullptr) const {
+	// project ray direction onto plane normal
+	// this should give us a value between -1 and 1
+	float t = direction.dot(plane.N);
+	// must face the plane
+	if (t > 0)
+		return false;
+	// get distance of ray origin to the plane
+	float d = origin.dot(plane.N) + plane.d;
+	// check if ray is parallel with the plane
+	// no intersection if parallel and not touching
+	if (t == 0 &&
+		d != 0)
+		return false;
+	// calculate distance along ray to plane
+	t = d == 0 ? 0 : -(d / t);
+	// intersects if within range
+	if (t >= 0 &&
+		t <= length) {
+		// store intersection point if requested
+		if (I != nullptr)
+			*I = origin + direction * t;
+		return true;
+	}
+	// default no intersection
+	return false;
+}
+
+bool Ray::intersects(const AABB& aabb, Vector2* I) const
+{
+	// get distances to each axis of the box
+	float xmin, xmax, ymin, ymax;
+	// get min and max in the x-axis
+	if (direction.x < 0) {
+		xmin = (aabb.max.x - origin.x) / direction.x;
+		xmax = (aabb.min.x - origin.x) / direction.x;
+	}
+	else {
+		xmin = (aabb.min.x - origin.x) / direction.x;
+		xmax = (aabb.max.x - origin.x) / direction.x;
+	}
+	// get min and max in the y-axis
+	if (direction.y < 0) {
+		ymin = (aabb.max.y - origin.y) / direction.y;
+		ymax = (aabb.min.y - origin.y) / direction.y;
+	}
+	else {
+		ymin = (aabb.min.y - origin.y) / direction.y;
+		ymax = (aabb.max.y - origin.y) / direction.y;
+	}
+	// ensure within box
+	if (xmin > ymax || ymin > xmax)
+		return false;
+	// the first contact is the largest of the two min
+	float t = max(xmin, ymin);
+	// intersects if within range
+	if (t >= 0 && t <= length) {
+		// store intersection point if requested
+		if (I != nullptr)
+			*I = origin + direction * t;
+		return true;
+	}
+	// not within Ray's range
+	return false;
+}
---
+++ MathsLib/Ray.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "Vector2.h"
+#include <cmath>
+#include "Sphere.h"
+#include "Plane.h"
+
+// ray where direction should be normalised
+class Ray {
+public:
+	Ray() {}
+	// default infinite length
+	Ray(const Vector2& start,
+		const Vector2& dir,
+		float l = INFINITY)
+		: origin(start), direction(dir), length(l) {}
+
+	Vector2 closestPoint(const Vector2& point) const;
+	bool intersects(const Sphere& sphere, Vector2* I) const;
+	bool intersects(const Plane& plane, Vector2* I) const;
+	bool intersects(const AABB& aabb, Vector2* I = nullptr) const;
+	Vector2 origin;
+	Vector2 direction;
+	float length;
+	
+};
+
